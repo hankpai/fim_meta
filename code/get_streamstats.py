30a90e770743de294d3d9cc31c08f7ce683bf1fd
henry.pai <henry.pai@noaa.gov>
2024-09-07 22:41:40 -0700

    functional usgs and nwm aep stats, test loop

--- code/get_streamstats.py
+++ code/get_streamstats.py
@@ -2,12 +2,27 @@
 # contact info:         henry <dot> pai <at> noaa <dot> gov
 # last edit by:         hp
 # last edit time:       Sep 2024
+# last edit comment:    start of USGS and NWM AEP, note NWM stats from USGS Bulletin 17C eq. 11
 
-# example usgs streamstats call (taken from nws gage analysis): https://streamstats.usgs.gov/gagestatsservices/statistics?statisticGroups=pfs&stationIDOrCode=14191000
-# simplest resource for extracting zarr data (hopefully same for NWM v3.0): https://www.hydroshare.org/resource/c4c9f0950c7a42d298ca25e4f6ba5542/
-# other resource for v3: https://www.hydroshare.org/resource/6ca065138d764339baf3514ba2f2d72f/
-# nwm retrospective: https://registry.opendata.aws/nwm-archive/
-# nwm variables: https://github.com/NOAA-Big-Data-Program/bdp-data-docs/blob/main/nwm/README.md
+# summary:
+# aggregates AEP stats from USGS and NWM; note NWM stats from USGS Bulletin 17C eq. 11
+
+# things to be careful about:
+# - NWM and AEP calc may have different methods (local regression vs the equation listed in summary)
+
+# inputs:
+# - usgs streamstats, example call (from nws gage analysis): https://streamstats.usgs.gov/gagestatsservices/statistics?statisticGroups=pfs&stationIDOrCode=14191000
+#   - use USGS's dataretrieval?: https://doi-usgs.github.io/dataretrieval-python/
+# - nwm AEP stats
+#   - resource for extracting zarr data (hopefully same for NWM v3.0): https://www.hydroshare.org/resource/c4c9f0950c7a42d298ca25e4f6ba5542/
+#   - other resource for v3: https://www.hydroshare.org/resource/6ca065138d764339baf3514ba2f2d72f/
+#   - nwm retrospective: https://registry.opendata.aws/nwm-archive/
+#   - nwm variables: https://github.com/NOAA-Big-Data-Program/bdp-data-docs/blob/main/nwm/README.md
+
+# TODO
+# [ ] look at xarray group by, can this be done by coordinate?
+# [ ] perhaps use USGS dataretrieval package, now in python!
+# [ ] need stats on annual peaks?
 
 import os
 import yaml
@@ -15,10 +30,11 @@ import pathlib
 import glob
 import pandas as pd
 import numpy as np
+import scipy
 import json
 import logging
-import urllib
 import urllib3
+import xarray as xr
 import fsspec
 import pdb
 
@@ -67,11 +83,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)-4s %(message)s',
 # ===== functions
 def org_usgs(usgs_json):
     """
-    ['id', 'statisticGroupTypeID', 'regressionTypeID', 'stationID', 'value',
-       'unitTypeID', 'comments', 'isPreferred', 'citationID',
-       'predictionIntervalID', 'statisticErrors', 'statisticGroupType',
-       'regressionType', 'predictionInterval', 'yearsofRecord'],
-      dtype='object')
+    pulls in relevant usgs streamstats for thresholds listed in the global var aep_li
     """
     temp_df = pd.DataFrame(usgs_json)
     # taking preferred USGS AEP, note yearsofRecord only taken from empirical AEP (vs. regression/algorithmic AEP)
@@ -79,6 +91,7 @@ def org_usgs(usgs_json):
     pref_df = temp_df[temp_df['isPreferred']==True][['value', 'yearsofRecord', 'citationID', 'regressionType']]
 
     stats_meta = pd.DataFrame(list(pref_df['regressionType']))
+
     # removes AEP, then splits by PK, then replaces underscore with decimal
     aep_percent = stats_meta['code'].str.rstrip('AEP')\
                                     .str.split('PK', expand=True)[1]\
@@ -90,23 +103,80 @@ def org_usgs(usgs_json):
     org_df['aep_percent'] = aep_percent
     org_df['aep_name'] = stats_meta.iloc[row_idxs]['name']
 
+    org_df.rename(columns={'value':'usgsFlow_cfs'}, inplace=True)
+
     return_df = org_df
     return(return_df)
+
+def org_nwm(nwm_ds, water_yr):
+    """
+    solves for Eq 11 for USGS Bulletin 17C, Chapter 5 of Book 4 for AEP estimates.  This should be similar to WRDS estimates, but does NOT perform
+    low-outlier tests
+
+    quote from WRDS site: https://vlab.noaa.gov/redmine/projects/wrds/wiki/WRDS_Location_API
+
+    'Streamflow Annual Exceedance Probabilities (AEP) were calculated for 2, 5, 10, 25, 50, and 100 year return periods. The streamflow AEPs were
+    estimated using methods outlined in the USGS Bulletin 17C. Annual peak flows were derived from systematic modelled output from the NWM v2.1
+    41-year retrospective and these were then used with the "Parameter Estimation—Simple Case" (USGS Bulletin 17C, page 25) with MGBT
+    (Multiple Grubbs-Beck Low-Outlier Test) to identify and remove PILF (Potentially Influential Low Flows), and a frequency factor (k) which is a
+    function of the skew coefficient (G).'
+    """
+    m3_to_f3 = 100**3 / (2.54**3) / (12**2)
+    yr_pks = nwm_ds.groupby(water_yr).max().values # time consuming step & heavy download; also groupby coordinate?
+    mean_pks = yr_pks.mean()
+    std_pks = np.std(yr_pks)
+    skew_pks = scipy.stats.skew(yr_pks)
+
+    alpha = 4 / (skew_pks**2)
+    beta = np.sign(skew_pks) * (((std_pks**2) / alpha)**0.5)
+    tau = mean_pks - alpha * beta
+
+    q_li = [1 - float(aep_str)/100 for aep_str in aep_li]
+    x_q_li = [round((tau + beta * (scipy.special.gammaincinv(alpha, q))) * m3_to_f3) for q in q_li]
+
+    return_df = pd.DataFrame()
+    return_df['aep_percent'] = aep_li
+    return_df['nwmFlow_cfs'] = x_q_li
+
+    return(return_df)
     
-def get_site_info(mapping_df, request_header):
+def get_site_info(mapping_df, request_header, aoi, ds):
     """
     loop through getting usgs streamstats and attempted NWM retrospective v3 streamstats
     """
     loop_li = []
-    for i, row in mapping_df.iterrows():
-        usgs_url = usgs_url_prefix + str(row['usgs_gage'])
+    http = urllib3.PoolManager()
+    esternal_count = 0
+    for i, row in mapping_df.iloc[start_index:].iterrows():
+        if row.usgs_gage > 0: # value is zero if usgs_gage is missing
+            usgs_url = usgs_url_prefix + str(row.usgs_gage)
+            usgs_response = http.request('GET', usgs_url, headers=request_header)
+            usgs_json = json.loads(usgs_response.data.decode('utf8'))
+            usgs_df = org_usgs(usgs_json)
+        else:
+            usgs_df = pd.DataFrame(columns=['usgsFlow_cfs','yearsofRecord', 'citationID', 'aep_percent', 'aep_name'])
+            logging.info(row.ahps_lid + ' is not related to a usgs gage, so no gage stats')
 
-        http = urllib3.PoolManager()
-        usgs_response = http.request('GET', usgs_url, headers=request_header)
-        usgs_json = json.loads(usgs_response.data.decode('utf8'))
-        usgs_df = org_usgs(usgs_json)
-        pdb.set_trace()
+        # as of 2024 Sep, the retro run goes from 1979 Feb to 2023 Feb
+        nwm_ds = ds.sel(feature_id=row.nwm_seg)['streamflow'].sel(time=slice('1979-10-01', '2022-09-30'))
+        water_yr = (nwm_ds.time.dt.month >=10) + nwm_ds.time.dt.year
+        nwm_ds.coords['water_yr'] = water_yr # https://stackoverflow.com/questions/72268056/python-adding-a-water-year-time-variable-in-an-x-array
+        nwm_df = org_nwm(nwm_ds, water_yr)
 
+        site_df = nwm_df.merge(usgs_df, left_on='aep_percent', right_on='aep_percent', how='left')
+
+        print(str(i) + ' : ' + aoi + ' - ' + row.ahps_lid + ' = ' + str(row.usgs_gage))
+        if external_count == 0 and start_index == 0:
+            site_df.to_csv(os.path.join(out_dir, out_fn_prefix + aoi + out_fn_suffix), index=False)
+        else:
+            site_df.to_csv(os.path.join(out_dir, out_fn_prefix + aoi + out_fn_suffix), index=False, mode='a', header=False)
+
+        loop_li.append(site_df)
+
+    logging.info('scraping done')
+    return_df = pd.concat(loop_li)
+    return(return_df)
+    
 def main():
     with open(os.path.join(ctrl_dir, yaml_fn)) as f:
     # NWRFC settings for request headers, keeping hidden in yaml file
@@ -115,6 +185,10 @@ def main():
     
     areas_df = pd.read_csv(os.path.join(ctrl_dir, areas_fn))
     aois_li = areas_df.loc[areas_df['include'] == 'x']['area'].tolist()
+
+    logging.info('loading begun for NWM retro bucket')
+    ds = xr.open_zarr(fsspec.get_mapper(nwm_retro_bucket_url, anon=True),consolidated=True)
+    logging.info('loading complete for NWM retro bucket')
 
     for aoi in aois_li:
         logging.info(aoi + ' streamstats gathering has started')
@@ -125,7 +199,11 @@ def main():
                                                               'nwm_seg', 
                                                               'usgs_gage']]
 
-        get_site_info(usgs_map_df, request_header)
+        stats_df = get_site_info(usgs_map_df, request_header, aoi, ds)
+
+        logging.info(aoi + ' streamstats gathering has finished')
+    
+    logging.shutdown()
 
 if __name__ == '__main__':
     main()
